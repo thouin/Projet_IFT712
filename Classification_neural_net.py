9ac8ec88f70ba92b1d5f5f85ffbf5463005d9748
Kevin Thouin <Kevin.Thouin@USherbrooke.ca>
2019-12-10 22:34:41 -0500

    Fixed intendation error

--- Classification_neural_net.py
+++ Classification_neural_net.py
@@ -10,7 +10,7 @@ class neural_net:
         self.max_iter = max_iter
 
     def __epoch(x_train, y_train, x_valid, y_valid, num_classes=3):
-	classes = np.aranges(num_classes)
+        classes = np.aranges(num_classes)
         self.model.partial_fit(x_train, y_train, classes)
         # On calcule la loss pour les données de validation et d'entraînement
         t_train_pred = self.model.predict_proba(x_train)
@@ -18,10 +18,10 @@ class neural_net:
         t_valid_pred = self.model.predict_proba(x_valid)
         valid_loss = log_loss(y_valid, y_valid_pred, normalize=True, labels=classes)
         # On calcule la justesse pour les données de validation et d'entraînement
-	y_train_pred = argmax(t_train_pred, axis=1)
-	train_accu = (y_train_pred == y_train).mean()
-	y_valid_pred = argmax(t_valid_pred, axis=1)
-	valid_accu = (y_valid_pred == y_valid).mean()
+        y_train_pred = argmax(t_train_pred, axis=1)
+        train_accu = (y_train_pred == y_train).mean()
+        y_valid_pred = argmax(t_valid_pred, axis=1)
+        valid_accu = (y_valid_pred == y_valid).mean()
         return train_loss, train_accu, valid_loss, valid_accu
 
     def entrainement(self, x_train, y_train, x_valid, y_valid):
