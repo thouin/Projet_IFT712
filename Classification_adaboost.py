da49fdae1c1c2d9bbe6a219d01eff1cce0067c79
Kevin Thouin <Kevin.Thouin@USherbrooke.ca>
2019-12-11 00:21:51 -0500

    Prediction entrainement

--- Classification_adaboost.py
+++ Classification_adaboost.py
@@ -3,3 +3,10 @@ from sklearn.ensemble import AdaBoostClassifier
 class adaboost:
     def __init__(self, n_estimators, lr):
         self.model = AdaBoostClassifier(n_estimators=n_estimators, learning_rate=lr)
+        
+    def entrainement(self, X_train, Y_train):
+        self.model.fit(X_train, Y_train)
+
+    def prediction(self, X_test):
+        a = self.model.predict(X_test)
+        return a
