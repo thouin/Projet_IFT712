4ab3c179d72e5953a62d09f906968b9fb53c5c90
Kevin Thouin <Kevin.Thouin@USherbrooke.ca>
2019-12-11 17:24:10 -0500

    Added __epoch

--- Classification_logistique.py
+++ Classification_logistique.py
@@ -15,6 +15,21 @@ class Regression_Logistique:
         pred = self.model.predict_proba(data)
         return -log_loss(target, pred, labels=np.arange(3)) # TODO: Add regularisation term
 
+    def __epoch(x_train, y_train, x_valid, y_valid, num_classes=3):
+        classes = np.aranges(num_classes)
+        self.model.partial_fit(x_train, y_train, classes)
+        # On calcule la loss pour les données de validation et d'entraînement
+        t_train_pred = self.model.predict_proba(x_train)
+        train_loss = log_loss(y_train, y_train_pred, normalize=True, labels=classes)
+        t_valid_pred = self.model.predict_proba(x_valid)
+        valid_loss = log_loss(y_valid, y_valid_pred, normalize=True, labels=classes)
+        # On calcule la justesse pour les données de validation et d'entraînement
+        y_train_pred = argmax(t_train_pred, axis=1)
+        train_accu = (y_train_pred == y_train).mean()
+        y_valid_pred = argmax(t_valid_pred, axis=1)
+        valid_accu = (y_valid_pred == y_valid).mean()
+        return train_loss, train_accu, valid_loss, valid_accu
+
     def entrainement(self, x_train, x_train):
         self.fit(x_train, y_train).score(x_train, y_train), self.model.score(x_train, t_train)
 
