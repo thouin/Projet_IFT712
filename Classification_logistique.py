0db2949c6be28d684d9704a4af5256b9eb92636c
Kevin Thouin <Kevin.Thouin@USherbrooke.ca>
2019-12-11 17:17:02 -0500

    Split Classification_Models.py

--- Classification_logistique.py
+++ Classification_logistique.py
@@ -1,29 +1,6 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.metrics import hinge_loss
 from sklearn.metrics import log_loss
-from numpy import arange
-
-class SVM_Sigmoide_Kernel:
-    def __init__(self):
-        print("-------- Application de SVM avec sigmoide --------")
-        self.model = SVC(kernel='sigmoid',gamma='scale')
-
-    def fit(self, data, target):
-        self.model.fit(data, target)
-        return self
-        
-    def score(self, data, target):
-        pred = self.model.descision_function(data)
-        return -hinge_loss(target, pred, labels=np.arange(3)) # TODO: Add regularisation term
-
-    def entrainement(self, x_train, x_train):
-        return self.fit(x_train, y_train).score(x_train, y_train), self.model.score(x_train, t_train)
-
-    def prediction(self, x):
-        a = self.model.predict(x)
-        return a
-
+import numpy as np
 
 class Regression_Logistique:
     def __init__(self):
@@ -44,4 +21,3 @@ class Regression_Logistique:
     def prediction(self, x):
         a = self.model.predict(x)
         return a
-    
