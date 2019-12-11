2f29177048dfc2b9eeb8d920707ea4f42ffca7a0
Kevin Thouin <Kevin.Thouin@USherbrooke.ca>
2019-12-11 11:42:29 -0500

    Ajusted Regression_logistique

--- Classification_Models.py
+++ Classification_Models.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import hinge_loss
+from sklearn.metrics import log_loss
 from numpy import arange
 
 class SVM_Sigmoide_Kernel:
@@ -28,15 +29,19 @@ class Regression_Logistique:
     def __init__(self):
         print("-------- Application de la régression linéaire --------")
         self.model = LogisticRegression(solver='lbfgs', multi_class='auto',max_iter=10000)
+        
+    def fit(self, data, target):
+        self.model.fit(data, target)
+        return self
+        
+    def score(self, data, target):
+        pred = self.model.predict_proba(data)
+        return -log_loss(target, pred, labels=np.arange(3)) # TODO: Add regularisation term
 
-    def entrainement(self, X_train, Y_train):
-        self.model.fit(X_train, Y_train)
+    def entrainement(self, x_train, x_train):
+        self.model.fit(x_train, y_train).score(x_train, y_train)
 
-    def prediction(self, X_test):
-        a = self.model.predict(X_test)
+    def prediction(self, x):
+        a = self.model.predict(x)
         return a
-
-    @staticmethod
-    def erreur(Y_test, Y_predict):
-        err_rate = (Y_predict != Y_test).mean()
-        return err_rate
+    
