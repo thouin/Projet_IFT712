dfbadf27bdbb336a3c0b9e55aafb9631cb499101
Kevin Thouin <thok2602@dinf-1023-27b.DInf.fsci.usherbrooke.ca>
2019-12-12 13:48:34 -0500

    renamed variables in main

--- Classification_main.py
+++ Classification_main.py
@@ -7,21 +7,21 @@ from Classification_io import io
 import Classification_hyperparameter as ch
 import Classification_io as ci
 
-def logistique(X_train, X_test, Y_train, Y_test):
+def logistique(x_train, x_test, y_train, y_test):
     print("-------- Application de la régression linéaire --------")
     est, params = ch.HyperparameterLogistique(x_train, y_train)
     train_loss_list, train_accu_list, test_loss_list, test_accu_list = est.entrainement(x_train, y_train, x_test, y_test)
     ci.io.plot(train_loss_list, train_accu_list, valid_loss_list, valid_accu_list, 'logistique.png')
     ci.io.print_params(params, 'logistique_param.txt')
 
-def svm(X_train, X_test, Y_train, Y_test):
+def svm(x_train, x_test, y_train, y_test):
     print("-------- Application de SVM avec sigmoide --------")
     est, params = ch.HyperparameterSVM(x_train, y_train)
     train_loss, train_accu, test_loss, test_accu = est.entrainement(x_train, y_train, x_test, y_test)
     ci.io.print_params(params, 'svm_param.txt')
     ci.io.print_errors(train_loss, train_accu, test_loss, test_accu, 'svm_error.txt')
 
-def neural_net(X_train, X_test, Y_train, Y_test):
+def neural_net(x_train, x_test, y_train, y_test):
     print("-------- Application de la régression linéaire --------")
     est, params = ch.HyperparameterNeuralNet(x_train, y_train, hidden_layers=(6, 6))
     train_loss_list, train_accu_list, test_loss_list, test_accu_list = est.entrainement(x_train, y_train, x_test, y_test)
@@ -33,14 +33,14 @@ def neural_net(X_train, X_test, Y_train, Y_test):
     ci.io.plot(train_loss_list, train_accu_list, valid_loss_list, valid_accu_list, 'neural_net66.png')
     ci.io.print_params(params, 'neural_net66_param.txt')
 
-def adaboost(X_train, X_test, Y_train, Y_test):
+def adaboost(x_train, x_test, y_train, y_test):
     print("-------- Application de adaboost avec un arbre de décision de profondeur un --------")
     est, params = ch.HyperparameterAdaboost(x_train, y_train)
     train_accu, test_accu = est.entrainement(x_train, y_train, x_test, y_test)
     ci.io.print_params(params, 'adaboost_param.txt')
     ci.io.print_scores(train_accu, test_accu, 'adaboost_error.txt')
 
-def bagging(X_train, X_test, Y_train, Y_test):
+def bagging(x_train, x_test, y_train, y_test):
     print("-------- Application de bagging --------")
     est, params = ch.HyperparameterBagging(x_train, y_train)
     train_accu, test_accu = est.entrainement(x_train, y_train, x_test, y_test)
