09361919577d870d98c3905fc6e5534c2af0a064
Kevin Thouin <thok2602@dinf-1023-27b.DInf.fsci.usherbrooke.ca>
2019-12-12 12:48:06 -0500

    neural_net

--- Classification_main.py
+++ Classification_main.py
@@ -18,6 +18,18 @@ def svm(X_train, X_test, Y_train, Y_test):
     ci.io.print_params(params, 'svm_param.txt')
     ci.io.print_errors(train_loss, train_accu, test_loss, test_accu, 'svm_error.txt')
 
+def neural_net(X_train, X_test, Y_train, Y_test):
+    print("-------- Application de la régression linéaire --------")
+    est, params = ch.HyperparameterNeuralNet(x_train, y_train, hidden_layers=(6, 6))
+    train_loss_list, train_accu_list, test_loss_list, test_accu_list = est.entrainement(x_train, y_train, x_test, y_test)
+    ci.io.plot(train_loss_list, train_accu_list, valid_loss_list, valid_accu_list, 'neural_net66.png')
+    ci.io.print_params(params, 'neural_net66_param.txt')
+
+    est, params = ch.HyperparameterNeuralNet(x_train, y_train, hidden_layers=(6))
+    train_loss_list, train_accu_list, test_loss_list, test_accu_list = est.entrainement(x_train, y_train, x_test, y_test)
+    ci.io.plot(train_loss_list, train_accu_list, valid_loss_list, valid_accu_list, 'neural_net66.png')
+    ci.io.print_params(params, 'neural_net66_param.txt')
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python Classification.py model\n")
