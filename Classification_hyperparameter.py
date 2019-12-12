383621433221f4e12d7338cab14f4d2a0b7d5f5c
Kevin Thouin <thok2602@dinf-1023-27b.DInf.fsci.usherbrooke.ca>
2019-12-12 10:56:17 -0500

    Added fit, best_param and best_estimator

--- Classification_hyperparameter.py
+++ Classification_hyperparameter.py
@@ -2,4 +2,13 @@ from sklearn.model_selection import GridSearchCV
 
 class HyperparameterSearch:
     def __init__(estimator, param_grid):
-        self.grid = GridSearchCV(estimator, param_grid, n_jobs=-1)	
+        self.grid = GridSearchCV(estimator, param_grid, n_jobs=-1)
+
+    def fit(x_train, y_train):
+        self.grid.fit(x_train, y_train)
+
+    def best_estimator():
+        return self.grid.best_estimator_
+
+    def best_params():
+        return self.grid.best_params_
